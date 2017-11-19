6409c0c51c2dd4095b85f5f05e34c1499da8b21d
somesame <863639156@qq.com>
2017-11-19 14:15:09 +0800

    Initial Commit

---
+++ Study&thought/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Study&thought
+//
+//  Created by lucid on 2017/11/19.
+//  Copyright © 2017年 Gooou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Study&thought/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Study&thought
+//
+//  Created by lucid on 2017/11/19.
+//  Copyright © 2017年 Gooou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
